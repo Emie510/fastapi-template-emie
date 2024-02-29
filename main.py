aa0003908e5363e8643d19c5cd55dbc766839106
Emie510 <ekleinholkenborg@redcross.nl>
2024-02-29 16:46:12 +0100

    another change

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class MyPayload(BaseModel):
 
 @app.post("/how cool is this?")
 async def post_how_cool_this_is(answer_to_question, dependencies=Depends(required_headers)):
-    """Provide your input on how cool this is"""
+    """Provide your input on how cool this is?"""
     
     # do something with input_data
     new_string = answer_to_question.text_field + ". Indeed, I agree. Supercool."
